07d1fb1181698be077fe0cc05530840c0c59a94e
haik-4a5a <460252@edu.rocmn.nl>
2017-03-29 21:16:29 +0200

    update

---
+++ comma/Snel.js
@@ -0,0 +1,7 @@
+function lijst() {
+var input = splitString(document.getElementById('invoer').value, comma);
+
+console.log(input);
+
+
+}
---
+++ idfunctie.js
@@ -0,0 +1,12 @@
+
+function id(elementId){
+
+var elementId = document.getElementById(elementId);
+
+return elementId;
+
+
+
+};
+
+ var elementId = id('een_id');
